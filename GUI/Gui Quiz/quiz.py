475e410ea270bef60a97889f260fd60cdd3de103
Hamish <24396@stduents.mrgs.school.nz>
2024-03-13 22:46:29 +1300

    Update Quiz class: added methods for setting up questions and handling score updates. Link confirm button to score update method.

--- GUI/Gui Quiz/quiz.py
+++ GUI/Gui Quiz/quiz.py
@@ -22,53 +22,73 @@ question_answers = {
         "type 'int'",
         "type 'type'",
         "Error",
-        "0"],
+        "0",
+        2],  
+
     2: ["What is the output of the following code : L = ['a','b','c','d'] print(''.join(L))",
         "Error",
         "None",
         "abcd",
-        "L"],
+        "L",
+        3],  
+
     3: ["What does the 'range' function in Python return?",
         "A list of integers",
         "A tuple of integers",
         "A generator object",
-        "An error"],
+        "An error",
+        1],  
+
     4: ["What is the result of 3*1**3?",
         "3",
         "9",
         "27",
-        "1"],
+        "1",
+        1],  
+
     5: ["Which of the following is not a valid variable name in Python?",
         "my-var",
         "_my_var",
         "myVar",
-        "MyVar"],
+        "MyVar",
+        1],  
+
     6: ["What is the output of the following code : 'hello'.upper()",
         "HELLO",
         "hello",
         "Error",
-        "'HELLO'"],
+        "'HELLO'",
+        1], 
+
     7: ["What will be the output of the following code : 'Python'[1:4]",
         "yth",
         "Pyt",
         "thon",
-        "Py"],
+        "Py",
+        1],  
+
     8: ["What is the result of 2**3**2?",
         "512",
         "64",
         "72",
-        "28"],
+        "28",
+        1],  
+
     9: ["Which of the following is used to create a comment in Python?",
         "#",
         "//",
         "/* */",
-        "<!-- -->"],
+        "<!-- -->",
+        1],  
+
     10: ["What is the output of the following code : 5 == 5.0",
          "True",
          "False",
          "Error",
-         "None of the above"]
+         "None of the above",
+         1]  
 }
+
 
 
 class QuizStarter:
@@ -78,7 +98,7 @@ class QuizStarter:
         self.quiz_frame.grid()
         
         self.heading_label = Label (self.quiz_frame,
-                                    text="GUI QUIZ",
+                                    text="Python Quiz",
                                     font=("Tw Cen MT", "18", "bold"),
                                     bg=background_color)
         self.heading_label.grid(row=0)
@@ -133,8 +153,61 @@ class Quiz:
         self.rb4.grid(row=4, sticky=W)
 
 
-        self.confirm_button = Button(self.quiz_frame, text="Confrim", bg="pink")
+        self.confirm_button = Button(self.quiz_frame, text="Confrim",font=("Helvetia", "13", "bold"), bg="SpringGreen3", command=self.text_progress)
         self.confirm_button.grid(row=5)
+
+        self.score_label = Label(self.quiz_frame, text="Score: 0", font=("Tw Cen MT", "14", "bold"),
+                                bg=background_color)
+        self.score_label.grid(row=6)
+
+    def questions_setup(self):
+            randomiser()
+            self.var1.set(0)
+            self.question_label.config(text=question_answers[qnum][0])
+            self.rb1.config(text=question_answers[qnum][1])
+            self.rb2.config(text=question_answers[qnum][2])
+            self.rb3.config(text=question_answers[qnum][3])
+            self.rb4.config(text=question_answers[qnum][4])
+
+
+    def text_progress(self):
+        global score
+        scr_label = self.score_label
+        choice = self.var1.get()
+        
+        if len(asked) >= len(question_answers):
+            if choice == int(question_answers[qnum][5]):
+                score += 1
+                scr_label.configure(text="Score: " + str(score))
+                self.confirm_button.config(text="Confirm")
+            else:
+                correct_answer = question_answers[qnum][5]
+                correct_answer = question_answers[qnum][correct_answer]
+                scr_label.configure(text="The correct answer was: " + str(correct_answer))
+                self.confirm_button.config(text="Confirm")
+        else:
+            if choice == 0:
+                self.confirm_button.config(text="Try again, You didn't select an option then submit again")
+                choice = self.var1.get()
+            else:
+                if choice == int(question_answers[qnum][5]):
+                    score += 1
+                    scr_label.configure(text="Score: " + str(score))
+                    self.confirm_button.config(text="Confirm")
+                    self.questions_setup()
+                else:
+                    correct_answer = question_answers[qnum][5]
+                    correct_answer = question_answers[qnum][correct_answer]
+                    scr_label.configure(text="The correct answer was: " + str(correct_answer))
+                    self.confirm_button.config(text="Confirm")
+                    self.questions_setup()
+
+
+
+
+        
+        
+        
 
         
         
@@ -148,7 +221,7 @@ class Quiz:
 
 if __name__ == "__main__":
     root = Tk()
-    root.title('GUI QUIZ')
+    root.title('Python Quiz')
     quizStarter_object = QuizStarter(root)
     root.mainloop()
     
